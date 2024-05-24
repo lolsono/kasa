46931d3701b3080812d48937014b6dfebc700784
lolsono <155098365+lolsono@users.noreply.github.com>
2024-05-24 12:19:35 +0200

    création du component footer
    -ajout diver de logo et image dans assets

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { LogoDark } from "./Logo.jsx";
+console.log(LogoDark);
+
+export function Footer() {
+  return (
+    <footer>
+      <LogoDark />
+      <p>© 2020 Kasa. All rights reserved</p>
+    </footer>
+  );
+}
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+export const LogoDesktop = () => {
+  return <img src="../assets/picture/logo-desktop.svg" alt="logo desktop" />;
+};
+
+export const LogoMobile = () => {
+  return <img src="../assets/picture/logo-mobile.svg" alt="logo mobile" />;
+};
+
+export const LogoDark = () => {
+  return <img src="../src/assets/picture/logo-black.svg" alt="logo black" />;
+};
