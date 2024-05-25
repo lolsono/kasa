91bac951e05ad9cc557a5a196ba68847b1989587
lolsono <155098365+lolsono@users.noreply.github.com>
2024-05-25 15:09:05 +0200

    ajout du component collapse

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,27 @@
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+
+function Collapse({ title, content }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse-container">
+      <button onClick={toggleCollapse}>
+        {title}{" "}
+        {isOpen ? (
+          <FontAwesomeIcon icon={faChevronDown} />
+        ) : (
+          <FontAwesomeIcon icon={faChevronUp} />
+        )}
+      </button>
+      {isOpen && <div>{content}</div>}
+    </div>
+  );
+}
+
+export default Collapse;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,15 @@
+import Collapse from "../components/Collapse.jsx";
 function About() {
   return (
-    <div>
-      <p>La Pages a propos</p>
+    <div className="about-container">
+      <img
+        src="../src/assets/picture/Image-source-2.png"
+        alt="Image des montages avec un lac"
+      />
+      <Collapse title="Fiabilité" content="Ceci est le contenu 1" />
+      <Collapse title="Respect" content="Ceci est le contenu 2" />
+      <Collapse title="Service" content="Ceci est le contenu 3" />
+      <Collapse title="Sécurité" content="Ceci est le contenu 3" />
     </div>
   );
 }
