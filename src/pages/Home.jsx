001c5914b994c2fdce07d78d8203456734953abe
lolsono <155098365+lolsono@users.noreply.github.com>
2024-05-25 14:19:11 +0200

    ajout des cards cover sur l acceuil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import DecorativeImage from "../components/DecorativeImage.jsx";
+
+import data from "../logements.json";
+
+function CardList() {
+  return (
+    <div className="cover-card">
+      {data.map((item) => (
+        <div key={item.id}>
+          <img src={item.cover} alt={item.title} />
+          <h2>{item.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 function Home() {
   return (
     <div>
       <DecorativeImage />
+      <CardList />
     </div>
   );
 }
