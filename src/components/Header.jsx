dc5e3a0f7e306bbf094f261cdb2ce8edeea633d3
lolsono <155098365+lolsono@users.noreply.github.com>
2024-06-27 22:39:46 +0200

    ajout des bordures pour la nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,35 @@ import { Link } from "react-router-dom";
 import { Logo } from "./Logo.jsx";
 
 function Header() {
+  function handleClick(event) {
+    const links = document.querySelectorAll(".link-text");
+
+    links.forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    event.target.classList.add("active");
+  }
+
   return (
     <header>
       <h1>
         <Logo />
       </h1>
       <nav>
-        <Link className="link-text link-acceuil" to="/">
+        <Link
+          className="link-text link-acceuil"
+          to="/"
+          activeClassName="active"
+          onClick={handleClick}
+        >
           ACCUEIL
         </Link>
-        <Link className="link-text" to="/about">
+        <Link
+          className="link-text link-about"
+          to="/about"
+          onClick={handleClick}
+        >
           A PROPOS
         </Link>
       </nav>
